0acfbdac402af67974caf00f3ac72cb24b176504
Semla <semla.dev@gmail.com>
2022-12-01 20:34:01 +0100

    using config values

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,6 @@
 import * as functions from "firebase-functions";
 
 import express = require('express');
-import http = require('http')
 import cors = require("cors")
 
 // export { Entsoe} from './Entsoe'
@@ -9,14 +8,13 @@ import cors = require("cors")
 // export { EntsoeConfig } from './interfaces/entsoeCache'
 
 import { Entsoe} from './Entsoe'
+import { Config} from './Config'
 // export { Config} from './Config'
 // export { EntsoeConfig } from './interfaces/entsoeCache'
 
 const app = express()
 app.use(cors({origin: true}));
-app.use(Entsoe.init({
-  securityToken: process.env.KEY as string
-}));
+app.use(Entsoe.init({...Config.get(), securityToken: process.env.KEY as string}));
 
 export const e = functions
     .region("europe-west1") // https://firebase.google.com/docs/functions/locations#web
